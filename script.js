a9e3e138715703d2a43ab3b71bacc64aa883e787
Romeu <romeu.esp@gmail.com>
2021-12-04 13:44:49 +0100

    :bug: Bugs correction

--- script.js
+++ script.js
@@ -2,35 +2,32 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
    document.querySelector(".container").classList.toggle("show-menu")
 );
 
-// orcamento -> qntde de paginas
-document.querySelector('#qtde').addEventListener("change", atualizarPreco);
-document.querySelector('#js').addEventListener("change", atualizarPreco);
-
+// calculo do orcamento de construcao de uma web 
+document.querySelector("#qtde").addEventListener("change", atualizarPreco);
+document.querySelector("#js").addEventListener("change", atualizarPreco);
 document.querySelector("#layout-sim").addEventListener("change", atualizarPreco);
 document.querySelector("#layout-nao").addEventListener("change", atualizarPreco);
 
 document.querySelector("#prazo").addEventListener("change", function () {
-   const prazo = document.querySelector('#prazo').value;
-   document.querySelector("label[for = prazo]").innerHTML = `Prazo: ${prazo} semanas`
-   atualizarPreco();
+   const prazo = document.querySelector("#prazo").value;
+   document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`;
+   atualizarPreco()
 });
 
+
 function atualizarPreco() {
-   const paginas = document.querySelector('#qtde').value;
-   const scriptJSincluido = document.querySelector('#js').checked;
-   const incluirLayout = document.querySelector('#layout-sim').checked;
-   let prazo = document.querySelector('#prazo').value;
+   const paginas = document.querySelector("#qtde").value;
+   const scriptJS = document.querySelector("#js").checked;
+   const layoutSim = document.querySelector("#layout-sim").checked;
+   let prazo = document.querySelector("#prazo").value;
 
-   precoFinal = 100 * paginas; // quatindade de paginas, o preco do script é o 10% de cada pagina criada
-   if (scriptJSincluido) precoFinal *= 1.1;
-   if (incluirLayout) precoFinal += 500;
+   let preco = paginas * 100;
+   if (scriptJS) preco *= 1.1;
+   if (layoutSim) preco += 500;
 
-   let taxaDeUrgencia = 1 - prazo * 0.1;
-   precoFinal *= 1 + taxaDeUrgencia;
+   let taxaUrgencia = 1 - prazo * 0.1;
+   preco *= 1 + taxaUrgencia;
 
-   precoFinal = precoFinal.toFixed(2);
-
-   console.log(precoFinal);
-   document.querySelector('#preco').innerHTML = `R$ ${precoFinal}`;
+   console.log(preco);
+   document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`;
 }
-
